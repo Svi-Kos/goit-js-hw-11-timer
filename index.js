52b9a7c0166f3847e94b48ca244ef9ba8d30c833
Svi-Kos <svitlanakos@gmail.com>
2020-10-23 12:49:36 +0300

    виносить змінну selectorEl

--- index.js
+++ index.js
@@ -43,23 +43,10 @@ const timer = new CountdownTimer({
 timer.start();
 
 function updateTimer({ days, hours, mins, secs, selector }) {
-  const refs = {
-    daysEl: document
-      .querySelector(`${selector}`)
-      .querySelector("span[data-value='days']"),
-    hoursEl: document
-      .querySelector(`${selector}`)
-      .querySelector("span[data-value='hours']"),
-    minsEl: document
-      .querySelector(`${selector}`)
-      .querySelector("span[data-value='mins']"),
-    secsEl: document
-      .querySelector(`${selector}`)
-      .querySelector("span[data-value='secs']"),
-  };
+  const selectorEl = document.querySelector(`${selector}`);
 
-  refs.daysEl.textContent = `${days}`;
-  refs.hoursEl.textContent = `${hours}`;
-  refs.minsEl.textContent = `${mins}`;
-  refs.secsEl.textContent = `${secs}`;
+  selectorEl.querySelector("span[data-value='days']").textContent = `${days}`;
+  selectorEl.querySelector("span[data-value='hours']").textContent = `${hours}`;
+  selectorEl.querySelector("span[data-value='mins']").textContent = `${mins}`;
+  selectorEl.querySelector("span[data-value='secs']").textContent = `${secs}`;
 }
